17a3fda138b0073174b7545d8a4daf7997ab9e7f
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-03-15 16:56:17 -0500

    Bump to version 0.1.3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="itopy",
-    version="0.1.2",
+    version="0.1.3",
     author="Jonatas Baldin",
     author_email="jonatas.baldin@gmail.com",
     packages=["itopy"],
     keywords="itop api cmdb",
     url="https://github.com/jonatasbaldin/itopy/",
     description="Library for manipulating iTOP CMDB/ITSM",
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "requests",
     ],
