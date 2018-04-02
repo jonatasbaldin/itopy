1fbb94afbf982ac9f5eca2ac93045680d7f77270
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-04-02 20:00:46 -0300

    Fix install bug

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name="itopy",
-    version="0.1.3",
+    version="0.1.4",
     author="Jonatas Baldin",
     author_email="jonatas.baldin@gmail.com",
     packages=["itopy"],
     keywords="itop api cmdb",
     url="https://github.com/jonatasbaldin/itopy/",
     description="Library for manipulating iTOP CMDB/ITSM",
-    long_description=open('README.md').read(),
     install_requires=[
         "requests",
     ],
